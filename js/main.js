d8cb67f8f8c6ac2e9aa07e159bfc0e524f23870d
Admin <admin@gmail.com>
2023-08-11 12:40:10 +0300

    main page

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+    $('#tel').inputmask('+7 (999) 999-99-99'); // Замените на нужную маску
+    $('#tel1').inputmask('+7 (999) 999-99-99'); // Замените на нужную маску
+    $('#tel2').inputmask('+7 (999) 999-99-99'); // Замените на нужную маску
+
+    var swiper = new Swiper('.utilization-messages-list', {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        loop: true,
+        navigation: {
+            nextEl: '.mess-button-next',
+            prevEl: '.mess-button-prev',
+        },
+    });
+
+    var swiper = new Swiper('.utilization-certificates-list', {
+        slidesPerView: 'auto',
+        spaceBetween: 20,
+        loop: true,
+        navigation: {
+            nextEl: '.cert-button-next',
+            prevEl: '.cert-button-prev',
+        },
+    });
+});
